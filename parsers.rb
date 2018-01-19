556777c5a773b7acb09014ca60b514edc7affac2
Justin Holzmann <jpholzmann@gmail.com>
2018-01-19 15:26:54 -0600

    Working on parsers with a decent workflow

---
+++ parsers.rb
@@ -0,0 +1,172 @@
+class Example
+  def self.run
+    Machine.new(
+      Add.new(Variable.new(:x), Variable.new(:y)),
+      { x: Number.new(3), y: Number.new(4) }
+    ).run
+  end
+end
+
+class Reset
+  def self.run
+    [:Machine, :Number, :Add, :Multiply, :Boolean, :LessThan, :GreaterThan, :Variable].each do |klass|
+      Object.send(:remove_const, klass)
+    end
+  end
+end
+
+class Machine < Struct.new(:expression, :environment)
+  def step
+    self.expression = expression.reduce(environment)
+  end
+
+  def run
+    while expression.reducible?
+      puts expression
+      step
+    end
+    puts expression
+  end
+end
+
+class Number < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def reducible?
+    false
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Add < Struct.new(:left, :right)
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def reduce(environment)
+    if left.reducible?
+      Add.new(left.reduce(environment), right)
+    elsif right.reducible?
+      Add.new(left, right.reduce(environment))
+    else
+      Number.new(left.value + right.value)
+    end
+  end
+
+  def reducible?
+    true
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Multiply < Struct.new(:left, :right)
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def reduce(environment)
+    if left.reducible?
+      Multiply.new(left.reduce(environment), right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce(environment))
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+
+  def reducible?
+    true
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Boolean < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def reducible?
+    false
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class LessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def reducible?
+    true
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reduce(environment)
+    if left.reducible?
+      LessThan.new(left.reduce(environment), right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce(environment))
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+end
+
+class GreaterThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} > #{right}"
+  end
+
+  def reducible?
+    true
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reduce(environment)
+    if left.reducible?
+      GreaterThan.new(left.reduce(environment), right)
+    elsif right.reducible?
+      GreaterThan.new(left, right.reduce(environment))
+    else
+      Boolean.new(left.value > right.value)
+    end
+  end
+end
+
+class Variable < Struct.new(:name)
+  def to_s
+    name.to_s
+  end
+
+  def reducible?
+    true
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reduce(environment)
+    environment[name]
+  end
+end
