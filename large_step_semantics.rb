0e0d4ec34cddee23d8d04ed980f6f4fcd5d74e8f
Justin Holzmann <c_jholzmann@groupon.com>
2018-02-16 16:04:16 -0600

    Add to_ruby functionality to large_step

--- large_step_semantics.rb
+++ large_step_semantics.rb
@@ -1,9 +1,12 @@
+$hook
+
 class Reset
   def self.run
     [:Example, :Number, :Add, :Multiply, :Boolean, :LessThan, :GreaterThan, :Variable,
-     :Assign, :If, :Sequence, :DoNothing
-    ].each do |klass|
-      Object.send(:remove_const, klass)
+     :Assign, :If, :Sequence, :DoNothing, :While
+    ].each do |klass_name|
+      klass = Module.const_get(klass_name)
+      Object.send(:remove_const, klass_name) if klass.is_a?(Class)
     end
     load "./large_step_semantics.rb"
     Example.run
@@ -12,10 +15,7 @@ end
 
 class Example
   def self.run
-    Sequence.new(
-      Assign.new(:x, Add.new(Number.new(3), Number.new(1))),
-      Assign.new(:y, Add.new(Variable.new(:x), Number.new(3))),
-    ).evaluate({})
+    $hook = Boolean.new(false).to_ruby
   end
 end
 
@@ -23,11 +23,19 @@ Number = Struct.new(:value) do
   def evaluate(environment)
     self
   end
+
+  def to_ruby
+    "-> e { #{value.inspect} }"
+  end
 end
 
 Boolean = Struct.new(:value) do
   def evaluate(environment)
     self
+  end
+
+  def to_ruby
+    "-> e { #{value.inspect} }"
   end
 end
 
@@ -35,11 +43,19 @@ Variable = Struct.new(:name) do
   def evaluate(environment)
     environment[name]
   end
+
+  def to_ruby
+    "-> e { e[#{name.inspect}] }"
+  end
 end
 
 Add = Struct.new(:left, :right) do
   def evaluate(environment)
     Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+  end
+
+  def to_ruby
+    "-> e { (#{left.to_ruby}).call(e) + (#{right.to_ruby}).call(e) }"
   end
 end
 
@@ -47,11 +63,19 @@ Multiply = Struct.new(:left, :right) do
   def evaluate(environment)
     Number.new(left.evaluate(environment).value * right.evaluate(environment).value)
   end
+
+  def to_ruby
+    "-> e { (#{left.to_ruby}).call(e) * (#{right.to_ruby}).call(e) }"
+  end
 end
 
 LessThan = Struct.new(:left, :right) do
   def evaluate(environment)
     Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
+  end
+
+  def to_ruby
+    "-> e { (#{left.to_ruby}).call(e) < (#{right.to_ruby}).call(e) }"
   end
 end
 
@@ -59,17 +83,29 @@ GreaterThan = Struct.new(:left, :right) do
   def evaluate(environment)
     Boolean.new(left.evaluate(environment).value > right.evaluate(environment).value)
   end
+
+  def to_ruby
+    "-> e { (#{left.to_ruby}).call(e) > (#{right.to_ruby}).call(e) }"
+  end
 end
 
 Assign = Struct.new(:name, :expression) do
   def evaluate(environment)
     environment.merge({ name => expression.evaluate(environment) })
   end
+
+  def to_ruby
+    "-> e { e.merge({ #{name.inspect} => (#{expression.to_ruby}).call(e) }) }"
+  end
 end
 
 class DoNothing
   def evaluate(environment)
     environment
+  end
+
+  def to_ruby
+    "-> e { e }"
   end
 end
 
@@ -82,10 +118,39 @@ If = Struct.new(:condition, :consequence, :alternative) do
       alternative.evaluate(environment)
     end
   end
+
+  def to_ruby
+    "-> e { if (#{condition.to_ruby}).call(e)" +
+    " then (#{consequence.to_ruby}).call(e)" +
+    " else (#{alternative.to_ruby}).call(e)" +
+    " end }"
+  end
 end
 
 Sequence = Struct.new(:first, :second) do
   def evaluate(environment)
     second.evaluate(first.evaluate(environment))
+  end
+
+  def to_ruby
+    "-> e { (#{second.to_ruby}).call((#{first.to_ruby}).call(e)) }"
+  end
+end
+
+While = Struct.new(:condition, :body) do
+  def evaluate(environment)
+    case condition.evaluate(environment)
+    when Boolean.new(true)
+      evaluate(body.evaluate(environment))
+    when Boolean.new(false)
+      environment
+    end
+  end
+
+  def to_ruby
+    "-> e {" +
+      " while (#{condition.to_ruby}).call(e); e = (#{body.to_ruby}).call(e); end;" +
+      " e" +
+      " }"
   end
 end
